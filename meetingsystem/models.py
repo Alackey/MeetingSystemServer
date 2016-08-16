37d62a59e4e34ce099ec8ec9026ca06b3220acaa
Anthony Lackey <alackey96@gmail.com>
2016-08-15 23:34:34 -0700

    Add Schedule model

--- meetingsystem/models.py
+++ meetingsystem/models.py
@@ -1,3 +1,4 @@
+from django.contrib.postgres.fields import ArrayField, JSONField
 from django.db import models
 
 
@@ -5,3 +6,8 @@ class User(models.Model):
     name = models.CharField(max_length=40)
     password = models.CharField(max_length=20)
     employeeID = models.CharField(max_length=20)
+
+
+class Schedule(models.Model):
+    employeeID = models.CharField(max_length=20, primary_key=True, unique=True)
+    blocks = ArrayField(JSONField())
